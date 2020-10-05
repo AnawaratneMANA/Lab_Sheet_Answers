7854ebb48f253e7473c65a2ce6d74798290f7b13
Nirmith Akash <IT19086408@my.sliit.lk>
2020-10-06 04:48:23 +0530

    Subject displaying interface implementation started Due how to display two text in the ListView.

--- app/src/main/java/com/example/crud_operation_practice/WelcomeStudent.java
+++ app/src/main/java/com/example/crud_operation_practice/WelcomeStudent.java
@@ -3,12 +3,21 @@ package com.example.crud_operation_practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class WelcomeStudent extends AppCompatActivity {
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_student);
+        list = findViewById(R.id.ListView);
+
+        //Get information to display.
+        //How to display subject name and the description both in the list view!?
+
+
     }
 }
