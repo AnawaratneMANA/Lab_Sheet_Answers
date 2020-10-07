008ba1b4da9ab3a962db6f1317d448d9817bc1e3
Nirmith Akash <IT19086408@my.sliit.lk>
2020-10-07 11:16:57 +0530

    RecycleView Created for the Student Message List with sample data

---
+++ app/src/main/java/com/example/crud_operation_practice/RecycleViewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.crud_operation_practice;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder>{
+    private static final String TAG = "RecycleViewAdapter";
+
+    private ArrayList<String> headings = new ArrayList<>();
+    private ArrayList<String> descriptions = new ArrayList<>();
+    private Context context;
+
+    public RecycleViewAdapter(ArrayList<String> headings, ArrayList<String> descriptions, Context context) {
+        this.headings = headings;
+        this.descriptions = descriptions;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_resource_file, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "OnBindViewHolder: called");
+        //Setting the Heading to the layout.
+        holder.heading.setText(headings.get(position));
+        holder.description.setText(descriptions.get(position));
+
+        //Onclick method
+        holder.parent_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Clicking method is working" + headings.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        //Amount of items in the arraylist if set to 0 nothing will be shown.
+        return headings.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView heading;
+        TextView description;
+        RelativeLayout parent_layout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            heading = itemView.findViewById(R.id.heading);
+            description = itemView.findViewById(R.id.description);
+            parent_layout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
--- app/src/main/java/com/example/crud_operation_practice/WelcomeStudent.java
+++ app/src/main/java/com/example/crud_operation_practice/WelcomeStudent.java
@@ -1,23 +1,50 @@
 package com.example.crud_operation_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import com.example.crud_operation_practice.RecycleViewAdapter;
+
+import java.util.ArrayList;
 
 public class WelcomeStudent extends AppCompatActivity {
 
-    private ListView list;
+    private static final String TAG = "MainActivity";
+
+
+    //Variables
+    private ArrayList<String> headings = new ArrayList<>();
+    private ArrayList<String> descriptions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_student);
-        list = findViewById(R.id.ListView);
+        Log.d(TAG, "onCreate: started");
+        initHeadingDesBind();
+    }
 
-        //Get information to display.
-        //How to display subject name and the description both in the list view!?
+    //Method
+    private void initHeadingDesBind(){
+        //Database calling method should be here.\
+        for(int i = 0; i<10; i++){
+            headings.add("Sample" + i);
+            descriptions.add("Sample" + i+2);
+        }
+        intiRecyclerView();
+    }
 
-
+    //Method to set the recyclerView
+    private void intiRecyclerView(){
+        Log.d(TAG, "initRecyclerView: inti recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.recycle_view);
+        RecycleViewAdapter adapter = new RecycleViewAdapter( headings, descriptions, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
